8ac0b8f31691fb994c1ea66613e84b0ba17b9a15
Seth Greylyn <seth.greylyn@gmail.com>
2017-12-17 15:05:48 -0800

    Added drawing behaviour to game file, attributes to Planets

--- src/game.js
+++ src/game.js
@@ -1,16 +1,41 @@
 import Planet from './planet';
-
+const planets = [];
+const pxPerUnit = 10;
+const dx = 2;
+const dy = 1;
 let canvas = document.getElementById("aether");
 let ctx = canvas.getContext("2d");
 
-ctx.beginPath();
-ctx.rect(20,40,50,50);
-ctx.fillStyle = '#FF0000';
-ctx.fill();
-ctx.closePath();
+function gameInit() {
+    planets.push(new Planet(canvas.width/2, canvas.height/2));
+}
 
-ctx.beginPath();
-ctx.arc(240, 160, 20, 0, 2*Math.PI, false);
-ctx.fillStyle = "green";
-ctx.fill();
-ctx.closePath();
+function move(planet) {
+    const pos = planet.getPosition();
+    const pixelRadius = planet.getRadius() * pxPerUnit;
+    let newX = pos.x + dx;
+    let newY = pos.y + dy;
+    if (newX > canvas.width - pixelRadius) {
+        newX = pixelRadius;
+    }
+    if (newY > canvas.height - pixelRadius) {
+        newY = pixelRadius;
+    }
+    planet.move(newX, newY);
+}
+
+function draw() {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    planets.forEach(planet => {
+        move(planet);
+        const pos = planet.getPosition();
+        const radius = planet.getRadius();
+        ctx.beginPath();
+        ctx.arc(pos.x, pos.y, radius * pxPerUnit, 0, 2*Math.PI, false);
+        ctx.fillStyle = "green";
+        ctx.fill();
+        ctx.closePath();
+    });
+}
+gameInit();
+setInterval(draw, 10);
--- src/planet.js
+++ src/planet.js
@@ -1,9 +1,28 @@
+/**
+ * A Planet object is the basic unit of the game. It is a circular body
+ * drawn on the canvas, with a radius, mass, and density which affect its
+ * gravitational attraction to other Planets.
+ */
 export default class Planet {
-    constructor () {
+    constructor (x, y) {
+        this.pos = {x, y};
+
+        // Experimental constants, will be dynamised later.
         this.mass = 1;
+        this.radius = 1;
+        this.area = Math.PI * Math.pow(this.radius, 2);
+        this.density = this.mass / this.volume;
     }
 
-    getMass() {
-        return this.mass;
+    getPosition() {
+        return this.pos;
+    }
+
+    getRadius() {
+        return this.radius;
+    }
+
+    move(newX, newY) {
+        this.pos.x = newX, this.pos.y = newY;
     }
 }
